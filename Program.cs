bc6ba6f4bc2db29fdaf5e9c6328798e47bb5b3d9
HironoBaba <88128407+HironoBaba@users.noreply.github.com>
2023-07-18 14:33:47 +0000

    [test] push protection private repo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("")
+Console.WriteLine("DefaultEndpointsProtocol=https;AccountName=stpushprotection;AccountKey=J1fQ8jUhNGiIs53IcDdA50BPAaD8PawK0mIKn2WoBImZ5waLldcwUFrselQWu5/Q7/czYocnROk++AStnUdeZA==;EndpointSuffix=core.windows.net")
